e2a0659c446acab0de2da5c78079ab547943e167
Emmanuel Iarussi <emmanueliarussi@gmail.com>
2026-08-05 09:08:26 -0300

    first commit

---
+++ tp1/ejercicio.js
@@ -0,0 +1,13 @@
+// La imagen que tienen que modificar viene en el parámetro image y contiene inicialmente los datos originales
+// es objeto del tipo ImageData ( más info acá https://mzl.la/3rETTC6  )
+// Factor indica la cantidad de intensidades permitidas (sin contar el 0)
+function dither(image, factor, algorithm)
+{
+    // completar
+}
+
+// Imágenes a restar (imageA y imageB) y el retorno en result
+function substraction(imageA,imageB,result) 
+{
+    // completar
+}
